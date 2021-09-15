f15d04eb02d7c0ce1d06c2ef69cdc94b7432a66d
TreeGITOnEarth01 <Almmy.9301@gmail.com>
2021-09-15 11:28:20 +0800

    Added shhTest java file

---
+++ GIT_sshTest/src/shhTest.java
@@ -0,0 +1,12 @@
+
+public class shhTest {
+	public shhTest() {
+		System.out.println("Are you calling me?");
+	}
+	
+	public static void main(String[] args) {
+		shhTest sht = new shhTest();
+		System.out.println("Yes, I am calling you!");
+		
+	}
+}
