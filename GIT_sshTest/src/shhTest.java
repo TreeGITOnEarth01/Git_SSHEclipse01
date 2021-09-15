9c304186e31ce651af3b1e385d05cdf12be68f67
TreeGITOnEarth01 <Almmy.9301@gmail.com>
2021-09-15 17:26:36 +0800

    Commit new changes - Overflow and Underflow

--- GIT_sshTest/src/shhTest.java
+++ GIT_sshTest/src/shhTest.java
@@ -7,6 +7,15 @@ public class shhTest {
 	public static void main(String[] args) {
 		shhTest sht = new shhTest();
 		System.out.println("Yes, I am calling you!");
-		
+		int maxValue = Integer.MAX_VALUE;
+		int minValue = Integer.MIN_VALUE;
+		System.out.println(maxValue);
+		System.out.println(minValue);
+		//overflow
+		System.out.println(maxValue + 1);
+		//underflow
+		System.out.println(minValue -1);
+		int _value = 2_147_4_8_364_7;
+		System.out.println("What is the underscore number: " + _value);
 	}
 }
